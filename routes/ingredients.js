ea420a04aa974c009753b4b500ddb0e3eb569d87
Victor Hazebrouck <hazebrouck.victor@gmail.com>
2023-12-11 00:11:41 +0100

    search ingredients by string

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -2,22 +2,26 @@ var express = require('express');
 var router = express.Router();
 const Ingredient = require('../models/ingredients')
 
-router.post('/', async (req, res, next)=> {
-    const {name, unit, imageURL, category, allergens} = req.body
+router.post('/', async (req, res, next) => {
+    const { name, unit, imageURL, category, allergens } = req.body
     const ingredient = new Ingredient({
-        "name": name, 
+        "name": name,
         "unit": unit,
         "imageURL": imageURL,
         "category": category,
         "allergens": allergens,
     })
     const response = await ingredient.save()
-    res.json({result: true, response})
+    res.json({ result: true, response })
 });
 
 router.get("/", async (req, res) => {
-    const response =  await Ingredient.find()
-    res.json({res: response})
- });
+    const response = await Ingredient.find()
+    res.json({ res: response })
+});
 
+router.get("/search", async (req, res) => {
+    const response = await Ingredient.find({ name: { $regex: new RegExp(req.body.name, 'i') } })
+    res.json({ res: response })
+});
 module.exports = router;
