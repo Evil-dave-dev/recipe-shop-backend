fd95ef95c7c69196381ceba28283517d89f4026a
David <evil.dave59@gmail.com>
2023-12-12 12:05:33 +0100

    fix password problem

--- routes/users.js
+++ routes/users.js
@@ -57,9 +57,11 @@ router.post("/signin", function (req, res) {
   // Check if the user has already been registered
   User.findOne({ name: req.body.name.toLowerCase() }).then((data) => {
     if (data) {
-      res.json({ result: true });
-    } else if (bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: false, error: "wrong password" });
+      if (bcrypt.compareSync(req.body.password, data.password)) {
+        res.json({ result: true });
+      } else {
+        res.json({ result: false, error: "wrong password" });
+      }
     } else {
       res.json({ result: false, error: "User don't exists" });
     }
