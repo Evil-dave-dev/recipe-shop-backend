c3d558cd1d379b7898bb33671ac35034101c96f9
Victor Hazebrouck <hazebrouck.victor@gmail.com>
2023-12-12 12:17:36 +0100

    image route

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 const Recipe = require('../models/recipes');
 const checkBody = require('../modules/checkBody');
+const uniqid = require('uniqid');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
-router.post('/', async (req, res, next)=> {
-    const {name, creator, imageURL, instructions, ingredients, dishType, preparationTime, difficulty, regime, tags} = req.body
+
+router.post('/', async (req, res, next) => {
+    const { name, creator, imageURL, instructions, ingredients, dishType, preparationTime, difficulty, regime, tags } = req.body
 
     //const validation = checkBody(req)
     //if(!validation.result) res.json(validation)
@@ -14,21 +18,40 @@ router.post('/', async (req, res, next)=> {
         "creator": creator,
         "imageURL": imageURL,
         "instructions": instructions,
-        "ingredients": ingredients.map(e => ({id: e.id, amount: e.amount })),
+        "ingredients": ingredients.map(e => ({ id: e.id, amount: e.amount })),
         "dishType": dishType,
         "preparationTime": preparationTime,
         "difficulty": difficulty,
         "regime": regime,
         "tags": tags,
     })
+
     const response = await recipe.save()
     const populatedRes = await Recipe.findById(response._id).populate('ingredients.id')
-    res.json({result: true, response: populatedRes})
+
+    res.json({ result: true, response: populatedRes })
 });
 
 router.get("/", async (req, res) => {
-   const response =  await Recipe.find().populate('ingredients.id').sort({ _id: -1 }) 
-   res.json({res: response})
+    const response = await Recipe.find().populate('ingredients.id').sort({ _id: -1 })
+    res.json({ res: response })
 });
+
+router.get("/find/tag=:tag", async (req, res) => {
+    const response = await Recipe.find({ tags: { $in: [req.params.tag] } }).populate('ingredients.id').sort({ _id: -1 })
+    res.json({ res: response })
+});
+
+router.post("/pictures", async (req, res) => {
+    console.log(req.files.picture.data)
+    const photoPath = `../tmp/${uniqid()}.jpg`;
+    const resultMove = await req.files.picture.data.mv(photoPath);
+
+    if(resultMove) res.json({result: false, error: "Server couldn't temporarily save result"})
+
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    fs.unlinkSync(photoPath);
+    res.json({ result: true, url: resultCloudinary.secure_url });
+})
 
 module.exports = router;
